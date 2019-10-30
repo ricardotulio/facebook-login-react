2134478b53a5b73746a67e2ac7b29233e8ef5c7d
Ricardo Tulio <ledo.tulio@gmail.com>
2019-10-30 10:36:13 +0000

    Set up the babel and react
    Set up the babel and react on webpack.

--- config/webpack.config.common.js
+++ config/webpack.config.common.js
@@ -1,10 +1,33 @@
 const webpack = require('webpack')
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '../public_html/js/'),
+    path: path.resolve(__dirname, '../public_html/'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 }
