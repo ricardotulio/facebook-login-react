6646fec916f237ec50d2e28a4c0e58bf04cc4adc
Ricardo Tulio <ledo.tulio@gmail.com>
2019-10-31 14:01:33 +0000

    Creates a file to export all components into src/app/common

---
+++ src/app/common/index.jsx
@@ -0,0 +1,11 @@
+import Button from './Button'
+import Input from './Input'
+import InputText from './InputText'
+import Password from './Password'
+
+export {
+  Button,
+  Input,
+  InputText,
+  Password,
+}
